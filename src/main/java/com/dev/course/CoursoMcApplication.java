0f074f8eb7ac80443f926db2f10cba331e3720a7
Lelton Borges <lelton.borges1@gmail.com>
2020-04-20 17:47:45 -0300

    Project created

---
+++ src/main/java/com/dev/course/CoursoMcApplication.java
@@ -0,0 +1,13 @@
+package com.dev.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursoMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/course/CoursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
