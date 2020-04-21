ca711f6a24f633f7dd47881bd8ff461fae372bdf
Lelton Borges <lelton.borges1@gmail.com>
2020-04-21 17:41:55 -0300

    Created instantiation operation

--- src/main/java/com/dev/course/CoursoMcApplication.java
+++ src/main/java/com/dev/course/CoursoMcApplication.java
@@ -1,12 +1,30 @@
 package com.dev.course;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoursoMcApplication {
+import com.dev.course.domain.Category;
+import com.dev.course.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CoursoMcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
